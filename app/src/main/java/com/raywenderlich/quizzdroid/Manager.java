eaeaba5435628b1408735b6b4873194d93798c0c
James Nocentini <james.nocentini@gmail.com>
2016-07-03 17:07:03 +0100

    Refactor Manager.java

--- app/src/main/java/com/raywenderlich/quizzdroid/Manager.java
+++ app/src/main/java/com/raywenderlich/quizzdroid/Manager.java
@@ -39,6 +39,8 @@ import com.couchbase.lite.javascript.JavaScriptViewCompiler;
 import com.couchbase.lite.listener.Credentials;
 import com.couchbase.lite.listener.LiteListener;
 import com.couchbase.lite.replicator.Replication;
+import com.couchbase.lite.support.FileDirUtils;
+import com.couchbase.lite.util.ZipUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -64,33 +66,33 @@ public class Manager {
     public Replication pull;
 
     protected Manager(Context context) {
-        Helper.copyAssetFolder(context.getAssets(), "quizzdroid.cblite2",
-                "/data/data/com.raywenderlich.quizzdroid/files/quizzdroid.cblite2");
 
-        // 1
-        com.couchbase.lite.Manager.enableLogging(com.couchbase.lite.util.Log.TAG, com.couchbase.lite.util.Log.VERBOSE);
-        com.couchbase.lite.Manager.enableLogging(com.couchbase.lite.util.Log.TAG_QUERY, com.couchbase.lite.util.Log.VERBOSE);
-        com.couchbase.lite.Manager.enableLogging(com.couchbase.lite.util.Log.TAG_LISTENER, com.couchbase.lite.util.Log.VERBOSE);
-        com.couchbase.lite.Manager.enableLogging(com.couchbase.lite.util.Log.TAG_ROUTER, com.couchbase.lite.util.Log.VERBOSE);
-        com.couchbase.lite.Manager.enableLogging(com.couchbase.lite.util.Log.TAG_VIEW, com.couchbase.lite.util.Log.VERBOSE);
-
-        // 2
         com.couchbase.lite.Manager manager = null;
         try {
+            // 1
             manager = new com.couchbase.lite.Manager(new AndroidContext(context), null);
+            // 2
+            database = manager.getExistingDatabase("quizzdroid");
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
-        // 3
-        try {
-            assert manager != null;
-            database = manager.getDatabase("quizzdroid");
         } catch (CouchbaseLiteException e) {
             e.printStackTrace();
         }
 
-        // 4
+        // 3
+        if (database == null) {
+            try {
+                ZipUtils.unzip(context.getAssets().open("quizzdroid.cblite2.zip"), manager.getContext().getFilesDir());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                database = manager.getDatabase("quizzdroid");
+            } catch (CouchbaseLiteException e) {
+                e.printStackTrace();
+            }
+        }
+
         Credentials credentials = new Credentials(null, null);
         View.setCompiler(new JavaScriptViewCompiler());
         Database.setFilterCompiler(new JavaScriptReplicationFilterCompiler());
